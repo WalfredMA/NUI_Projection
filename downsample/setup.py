9e084723caef58f3f1a3c376a177c10fbeb9571c
Wangfei MA <Wangfei.MA@ucsf.edu>
2019-10-11 13:31:33 -0700

    Update setup.py

--- downsample/setup.py
+++ downsample/setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
 	version="0.0.1",
 	author="Walfred MA",
 	author_email="wangfei.ma@ucsf.com",
-	description="downsampleing tool based on recapture model",
+	description="NUI projection tool based on recapture model",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/WalfredMA/Saturation-Projection-for-NUI-insertion-discovery/tree/master/downsample",
