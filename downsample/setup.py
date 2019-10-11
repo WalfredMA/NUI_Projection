83a0574345b54af4fecb5e7993bf92908b6190b4
Wangfei MA <Wangfei.MA@ucsf.edu>
2019-10-11 13:31:05 -0700

    Update setup.py

--- downsample/setup.py
+++ downsample/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 setuptools.setup(
-	name="downsampling",
+	name="NUI_projection",
 	version="0.0.1",
 	author="Walfred MA",
 	author_email="wangfei.ma@ucsf.com",
